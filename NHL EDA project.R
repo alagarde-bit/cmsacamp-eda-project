94585fd8a96e087f8dce9b532b0e6c13cda3eaab
alagarde-bit <66431443+alagarde-bit@users.noreply.github.com>
2020-06-09 16:13:58 -0400

    Add files via upload

---
+++ NHL EDA project.R
@@ -0,0 +1,22 @@
+install.packages("tidyverse")
+library(tidyverse)
+nhl <- read.csv("nhl_pit_sj_game6.csv")
+
+nhl$cumulative_shots <- cumsum(nhl$shot_attempt == 1)
+table(nhl$event)
+nhl_team_stats <- nhl %>% 
+  filter(event %in% c("blocked_shot", "faceoff", "giveaway", "hit", "shot")) %>% 
+  select(event, team) %>% 
+  ggplot(aes(x = event, fill = team)) + geom_bar(position = "dodge") + 
+  scale_fill_manual(values = c("yellow", "cyan4")) + 
+  labs(title = " 2016-2017 NHL Stanley Cup Finals Game 6 Team Stats",
+       x = "Game Statistics",
+       y = "Counts",
+       fill = "Team") + 
+  theme_classic() + 
+  theme(legend.position = "bottom",
+        plot.title = element_text(size = 17),
+        axis.title = element_text(size = 15)) +
+  scale_x_discrete(labels = c("Blocked Shots", "Faceoffs", "Giveaways", "Hits","Shots on Goal"))
+nhl_team_stats
+
